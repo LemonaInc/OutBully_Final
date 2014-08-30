f4c062e11f8496e3bde38431cc08b13cc83ef25c
Malik Imran <imran@focusteck.com>
2014-08-31 00:53:12 +0500

    Initial Commit

---
+++ Taxi/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Taxi
+//
+//  Created by Malik Imran on 8/31/14.
+//  Copyright (c) 2014 Malik.Imran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ TaxiTests/TaxiTests.m
@@ -0,0 +1,34 @@
+//
+//  TaxiTests.m
+//  TaxiTests
+//
+//  Created by Malik Imran on 8/31/14.
+//  Copyright (c) 2014 Malik.Imran. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TaxiTests : XCTestCase
+
+@end
+
+@implementation TaxiTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
